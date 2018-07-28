e63bb617c0a8c8aa45f973cb797962bbda4d750e
jmhig <jmhig@YOGA720>
2018-07-28 22:20:15 +0200

    Initial project

---
+++ src/main/java/com/higuera/houstatapp/HoustatAppApplication.java
@@ -0,0 +1,12 @@
+package com.higuera.houstatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoustatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoustatAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/higuera/houstatapp/web/MainController.java
@@ -0,0 +1,15 @@
+package com.higuera.houstatapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		
+		return "hello";
+	}
+}
