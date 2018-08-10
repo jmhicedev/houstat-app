4ba9beb9782059fbf0d1a443b71b4da7cc01343c
jmhig <jmhig@YOGA720>
2018-08-10 22:27:08 +0200

    @Component in ExecutorArchiverJob

--- src/main/java/com/higuera/houstat/job/ExecutorArchiverJob.java
+++ src/main/java/com/higuera/houstat/job/ExecutorArchiverJob.java
@@ -1,8 +1,11 @@
 package com.higuera.houstat.job;
 
+import org.springframework.stereotype.Component;
+
 /**
  * It gets information from external servers (idealista) and saves data into database
  * */
+@Component
 public class ExecutorArchiverJob {
 
 	public void execute() {
